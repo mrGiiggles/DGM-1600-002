d7f3e70c077123287accb384df74f5a8facec652
Unknown <banderson7370@gmail.com>
2018-01-22 10:10:06 -0700

    New file Text Adventure; Number Guesser updated

--- NumberGuesser/Assets/Script/GuesserScript.cs
+++ NumberGuesser/Assets/Script/GuesserScript.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class GuesserScript : MonoBehaviour {
 
-   public int max;
-   public int min;
-   private int guess;
+    public int max;
+    public int min;
+    private int guess;
     public int count;
 
 
 
     // Use this for initialization
-    private void Start () {
-     
+    private void Start() {
+
 
 
         print("Welcome to Number Guesser");
-        print("Pick a number between " + min +  " and " + max);
+        print("Pick a number between " + min + " and " + max);
 
         //is the value GUESS
         NextGuess();
@@ -25,40 +25,51 @@ public class GuesserScript : MonoBehaviour {
         print("UP Arrow for higher, DOWN Arrow for lower, ENTER if correct");
 
         max = max + 1;
-	}
+    }
 
     private void NextGuess() {
         //figure out how randomize gues*************************
         count--;
-        guess = (min + max) / 2;
+        guess = Random.Range(min, max);
         print("is the number " + guess + " ?");
     }
-    
+
 
     // Update is called once per frame
     void Update() {
 
-     
+
         //Up Arrow
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             min = guess;
             NextGuess();
         }
         //Down Arrow
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             max = guess;
             NextGuess();
 
         }
-    
-            //Enter Key
-            else if (Input.GetKeyDown(KeyCode.Return))
-            {
-                print("Computer win!");
 
-            }
-    //IF count is zero, human wins
+        //Enter Key
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("Computer win!");
+
         }
-	}
+        else if (count == 0)
+        {
+            print("Human wins this round!");
+        }
+        //IF count is zero, human wins
+    }
+    
+}
+
+	
 
 
---
+++ Text Adventure/Text Adventure/Assets/Scripts/Adventure.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventure : MonoBehaviour {
+
+    public enum States{ cell, window, gate };
+    public States myState;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
