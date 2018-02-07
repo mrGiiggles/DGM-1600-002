1e6c36ba868cb23a4b0210e53b7fd69bbc5a8ec8
Unknown <banderson7370@gmail.com>
2018-02-07 09:54:54 -0700

    Added Physics to brick and ball

---
+++ Brick Breaker/Brick Breaker/Assets/Script/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health;
+    private void OnCollisionEnter2D(Collision2D collision) {
+        health--;
+        //if our helth gets to zero, destry object
+        if (health <= 0) {
+            Destroy(gameObject);
+
+        }
+}
+}
