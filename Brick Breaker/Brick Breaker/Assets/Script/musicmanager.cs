e96e5e2fee3d28a9a64861814f4ee12f31314f7e
Unknown <banderson7370@gmail.com>
2018-02-09 09:56:04 -0700

    Singleton script, made prefabs

---
+++ Brick Breaker/Brick Breaker/Assets/Script/musicmanager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicmanager : MonoBehaviour {
+    public static musicmanager instance = null;
+
+    // Use this for initialization
+    void Awake () {
+		if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {         //if instance is not assigned
+            Destroy(this.gameObject);        // then assign instance to this obect
+        }                                    //then destroy this object
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	
+}
