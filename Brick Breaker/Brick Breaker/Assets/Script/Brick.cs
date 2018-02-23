7cc3bb85a6a7a07879d18c4bb63f5f4d0b7dd1bc
Unknown <banderson7370@gmail.com>
2018-02-23 10:00:29 -0700

    made brick sprite change when hit

--- Brick Breaker/Brick Breaker/Assets/Script/Brick.cs
+++ Brick Breaker/Brick Breaker/Assets/Script/Brick.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Health : MonoBehaviour {
+public class Brick : MonoBehaviour {
 
     public int health;
+    public Sprite[] sprites;
 
     private void Awake() {
         GameManager.brickCount++;
-     
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         health--;
+        GetComponent<SpriteRenderer>().sprite = sprites[health];
+
         //if our helth gets to zero, destroy object
         if (health <= 0) {
             Destroy(gameObject);
