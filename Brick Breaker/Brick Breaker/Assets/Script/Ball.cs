a9e1e2efe4084e0c0c3613b5d4673e7a7cdf677f
Unknown <banderson7370@gmail.com>
2018-02-26 10:08:46 -0700

    made more changers

--- Brick Breaker/Brick Breaker/Assets/Script/Ball.cs
+++ Brick Breaker/Brick Breaker/Assets/Script/Ball.cs
@@ -8,7 +8,7 @@ public class Ball : MonoBehaviour {
     public Vector2 startSpeed;
 
     private void Start() {
-        GetComponent<Rigidbody2D>().AddForce(startSpeed, ForceMode2D.Force);
+        GetComponent<Rigidbody2D>().AddForce(startSpeed, ForceMode2D.Impulse);
     }
     private void OnCollisionEnter2D(Collision2D collision) {
         AudioSource.PlayClipAtPoint(hitSound, new Vector3(0, 0, 0));
