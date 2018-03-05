a23d2fdef830db4947031ead2f4e89370b176223
Unknown <banderson7370@gmail.com>
2018-03-05 09:53:26 -0700

    Character movement animations

---
+++ Final Project/New Unity Project/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rigid;
+    public float speed;
+    private Animator anim;
+    private SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //check for button pushes
+        rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0), ForceMode2D.Force);
+        anim.SetFloat("HorizontalMove", Input.GetAxisRaw("Horizontal"));
+
+        if(Input.GetAxisRaw("Horizontal") < -0.1f) {
+            //flip sprite renderer
+            rend.flipX = true;
+
+        } else {
+            //unflip
+            rend.flipX = false;
+        }
+		
+	}
+    }
