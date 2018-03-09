b7afd01e5008362a14c12e2ea3fbad9007f4dc02
Unknown <banderson7370@gmail.com>
2018-03-09 10:01:00 -0700

    Made jump/Dash animation

--- Final Project/New Unity Project/Assets/Script/PlayerMovement.cs
+++ Final Project/New Unity Project/Assets/Script/PlayerMovement.cs
@@ -11,21 +11,23 @@ public class PlayerMovement : MonoBehaviour {
     public float speed;
     private Animator anim;
     private SpriteRenderer rend;
+    public float jump;
+    public bool isGrounded;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         rend = GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         //check for button pushes
         rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0), ForceMode2D.Force);
         anim.SetFloat("HorizontalMove", Input.GetAxisRaw("Horizontal"));
 
-        if(Input.GetAxisRaw("Horizontal") < -0.1f) {
+        if (Input.GetAxisRaw("Horizontal") < -0.1f) {
             //flip sprite renderer
             rend.flipX = true;
 
@@ -36,6 +38,23 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetButton("Fire1")) {
             anim.SetTrigger("ShootGo");
         }
-		
-	}
+
+        if (isGrounded && Input.GetButtonDown("Jump")) {
+            rigid.AddForce(new Vector2(0, jump), ForceMode2D.Force);
+        }
+
     }
+    /*void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.tag == "Ground") {
+            isGrounded = false;
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.transform.tag == "Ground") {
+            isGrounded = false;
+        }
+     }*/
+}
+    
