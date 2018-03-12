bb60805b9dfa0afc1ec7fef33e8eda6be688e38f
Unknown <banderson7370@gmail.com>
2018-03-12 10:06:52 -0600

    Worked on Dash, made particle effect

---
+++ Final Project/New Unity Project/Assets/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public int health;
+    public ParticleSystem deathParticle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("collided");
+        health--;
+        if (health <= 0) {
+            //Die
+            //DeathParticle
+            Instantiate(deathParticle);
+            Destroy(gameObject);
+        }
+    }
+}
