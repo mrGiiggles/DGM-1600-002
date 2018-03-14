fadb074024a51e5a13f5e93577f7d4fe77d7cb66
Unknown <banderson7370@gmail.com>
2018-03-14 00:42:47 -0600

    done with player movement

--- _Dash it/New Unity Project/Assets/Script/PlayerMovement.cs
+++ _Dash it/New Unity Project/Assets/Script/PlayerMovement.cs
@@ -2,26 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour {
 
     private Rigidbody2D rigid;
     public float speed;
     private Animator anim;
+    private SpriteRenderer rend;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
 
         //vetical and horizontal movement
         rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0), ForceMode2D.Force);
         rigid.AddForce(new Vector2(0, Input.GetAxis("Vertical") * speed), ForceMode2D.Force);
         anim.SetFloat("HorizontalMove", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("VerticalMove", Input.GetAxisRaw("Vertical"));
 
+        /* if (Input.GetAxisRaw("Horizontal") < -0.1f) {
+             //flip sprite renderer
+             rend.flipX = true;
+         } else {
+             //unflip
+             rend.flipX = false;
+
+         }*/
     }
 }
+
