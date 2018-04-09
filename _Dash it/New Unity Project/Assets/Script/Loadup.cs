3b935332d4d9b3ce769f39f8c91c4de54f4df9e5
Unknown <banderson7370@gmail.com>
2018-04-09 10:06:47 -0600

    load level script made

---
+++ _Dash it/New Unity Project/Assets/Script/Loadup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Loadup : MonoBehaviour {
+
+    public Manager manager;
+    public string level;
+
+    private void Start() {
+        manager = GameObject.Find("Manager").GetComponent<Manager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.GetComponent<PlayerMovement>()) 
+            manager.LoadLevel(level);
+        }
+
+
+}
--- _Dash it/New Unity Project/Assets/Script/Manager.cs
+++ _Dash it/New Unity Project/Assets/Script/Manager.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour {
     public Text scoreBoard;
     private int score;
 
 
-	public void IncrementScore( int value) {
+    public void IncrementScore(int value) {
         score += value;
         scoreBoard.text = score.ToString();
+    }
+
+
+    public void LoadLevel(string level) {
+        SceneManager.LoadScene(level);
+
+
     }
 }
